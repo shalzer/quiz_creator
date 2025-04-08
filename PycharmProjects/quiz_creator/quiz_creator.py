0a8a6b6f8a6f183c39f8210c3b64dc16582a6d42
KER AND HAN <shalcozbi@gmail.com>
2025-04-08 13:47:16 +0800

    created pseudocode

---
+++ PycharmProjects/quiz_creator/quiz_creator.py
@@ -0,0 +1,18 @@
+#Create a program that ask user for a question, it will also ask for 4 possible answer (a,b,c,d) and the correct answer.
+#Write the collected data to a text file. Ask another question until the user chose to exit.
+
+#set the file name to save the questions
+#start an infinite loop that allows user to add questions
+#ask user to input a new quiz question
+#collect the question and choices from the user
+#ask user to enter first, second, third, and fourth choice
+#prompt user for the correct answer
+
+#open the file in append mode to store question and choices
+#write the question and choices to the file
+#add a divider to separate questions
+
+#ask user if they want to add another question
+#if they answer "yes", tell the user that the quiz is saved
+#else, break the loop
+#exit loop
